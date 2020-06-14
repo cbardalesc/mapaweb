f454763340019edc94451ffac80a3b7960a2c8f4
Carlos Bardales <66852037+cbardalesc@users.noreply.github.com>
2020-06-13 21:35:15 -0500

    Add files via upload

---
+++ data/CasosConfirmadosPeru_1.js
@@ -0,0 +1,8 @@
+var json_CasosConfirmadosPeru_1 = {
+"type": "FeatureCollection",
+"name": "CasosConfirmadosPeru_1",
+"crs": { "type": "name", "properties": { "name": "urn:ogc:def:crs:OGC:1.3:CRS84" } },
+"features": [
+
+]
+}
